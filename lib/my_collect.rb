68b490c17079921222b273a6fdc31730e29574c8
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 18:46:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(array)
+  counter = 0
+  collected = []
 
-
+  while counter < array.length
+    collected[counter] = yield(array[counter])
+    counter += 1
+  end
+  collected
+end
